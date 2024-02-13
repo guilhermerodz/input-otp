da6d1d852255d3c391bc03ea4ea1af4cf22d52c4
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-02-13 18:42:57 -0300

    feat(otp-input): add current caret position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,11 +39,27 @@ const OTPInput = React.forwardRef<HTMLInputElement, OTPInputProps>(
     const [caretData, setCaretData] = React.useState<
       [number | null, number | null]
     >([null, null])
+    const clamppedCaretData = caretData.map(n =>
+      n === null ? n : Math.max(0, Math.min(n, maxLength)),
+    )
 
     function onInputSelect(e: React.SyntheticEvent<HTMLInputElement>) {
       const { selectionStart, selectionEnd } = e.currentTarget
 
       setCaretData([selectionStart, selectionEnd])
+    }
+
+    function setCaretPosition(start: number, end: number) {
+      if (!inputRef.current) {
+        return
+      }
+
+      inputRef.current.setSelectionRange(start, end)
+    }
+
+    function onInputFocus(e: React.SyntheticEvent<HTMLInputElement>) {
+      const lastPos = value.length
+      setCaretPosition(lastPos, lastPos)
     }
 
     function isSelected(slotIdx: number) {
@@ -55,8 +71,19 @@ const OTPInput = React.forwardRef<HTMLInputElement, OTPInputProps>(
       )
     }
 
+    function isCurrent(slotIdx: number) {
+      // TODO: simplify logic
+      return (
+        (clamppedCaretData[0] === slotIdx &&
+          clamppedCaretData[1] === slotIdx) ||
+        (slotIdx === maxLength - 1 &&
+          clamppedCaretData[0] === maxLength &&
+          clamppedCaretData[1] === maxLength)
+      )
+    }
+
     return (
-      <div>
+      <div className="relative">
         <div className="flex items-center gap-2">
           {Array.from({ length: maxLength }).map((_, idx) => {
             return (
@@ -64,6 +91,7 @@ const OTPInput = React.forwardRef<HTMLInputElement, OTPInputProps>(
                 key={idx}
                 className={cn('w-10 h-10 bg-white rounded-md', {
                   'bg-[green]': isSelected(idx),
+                  'bg-[yellow]': isCurrent(idx),
                 })}
               >
                 {paddedValue[idx]}
@@ -73,9 +101,14 @@ const OTPInput = React.forwardRef<HTMLInputElement, OTPInputProps>(
         </div>
 
         <input
+          className={cn(
+            'pointer-events-none',
+            // "absolute inset-0 opacity-0"
+          )}
           ref={inputRef}
           maxLength={maxLength}
           value={value}
+          onFocus={onInputFocus}
           onChange={e => onChange(e.target.value)}
           onSelect={onInputSelect}
           onBlur={() => setCaretData([null, null])}
