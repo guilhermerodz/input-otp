8e6ffad88670a0eb7d81258b37fa410ae81ac4fa
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-02-13 20:52:52 -0300

    chore(opt-input): make it similar to stripe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   }
 
   return (
-    <div className="w-[100dvw] h-[100dvh] bg-black text-black flex items-center justify-center">
+    <div className="w-[100dvw] h-[100dvh] bg-white text-black flex items-center justify-center [font-family:-apple-system,'system-ui','Segoe_UI',Roboto]">
       <OTPInput
         value={otpInputValue}
         onChange={setOtpInputValue}
@@ -151,16 +151,27 @@ const OTPInput = React.forwardRef<HTMLInputElement, OTPInputProps>(
 
     return (
       <div className="relative">
-        <div className="flex items-center gap-2" onClick={onContainerClick}>
+        <div
+          className="flex items-center gap-0 text-[2rem] leading-none"
+          onClick={onContainerClick}
+        >
           {Array.from({ length: maxLength }).map((_, idx) => {
             return (
               <div
                 key={idx}
                 onClick={e => onSlotClick(e, idx)}
-                className={cn('w-10 h-10 bg-white rounded-md', {
-                  'bg-[green]': isSelected(idx),
-                  'bg-[yellow]': isCurrent(idx),
-                })}
+                className={cn(
+                  'w-10 h-[52px] bg-white [--bsh-width:0px] transition-all border-slate-300 border-r border-y flex items-center justify-center',
+                  {
+                    '[box-shadow:0_0_0_var(--bsh-width)_rgb(147_197_253_/_.7)] border-blue-300 [--bsh-width:4px] z-10':
+                      isCurrent(idx) || isSelected(idx),
+                    '[&:nth-child(1)]:rounded-l-md [&:nth-child(1)]:border-l [&:nth-child(4)]:rounded-l-md [&:nth-child(4)]:border-l':
+                      true,
+                    '[&:last-child]:rounded-r-md [&:nth-child(3)]:rounded-r-md':
+                      true,
+                    'ml-10': idx === 3,
+                  },
+                )}
               >
                 {paddedValue[idx]}
               </div>
@@ -169,10 +180,7 @@ const OTPInput = React.forwardRef<HTMLInputElement, OTPInputProps>(
         </div>
 
         <input
-          className={cn(
-            'pointer-events-none',
-            "absolute inset-0 opacity-0"
-          )}
+          className={cn('pointer-events-none', 'absolute inset-0 opacity-0')}
           ref={inputRef}
           maxLength={maxLength}
           value={value}
