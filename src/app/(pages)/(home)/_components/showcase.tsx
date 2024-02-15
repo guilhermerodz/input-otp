15ec4e37a5f35a81d1f66f2c514b6a5d8a81c9cd
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-02-15 11:17:45 -0300

    chore(www): setup shadcn/ui

---
+++ src/app/(pages)/(home)/_components/showcase.tsx
@@ -0,0 +1,117 @@
+'use client'
+
+import React from 'react'
+import { useForm, Controller } from 'react-hook-form'
+
+import { OTPInput } from '@/app/component'
+import { cn } from '@/lib/utils'
+
+type FormValues = {
+  otp: string
+}
+
+export function Showcase({ className, ...props }: { className?: string }) {
+  const [formDisabled, setFormDisabled] = React.useState(false)
+
+  const {
+    control,
+    handleSubmit,
+    setFocus,
+    reset,
+    setValue,
+    formState,
+    register,
+  } = useForm<FormValues>({
+    defaultValues: {
+      otp: '12',
+    },
+    disabled: formDisabled,
+  })
+
+  React.useEffect(() => {
+    setFocus('otp')
+  }, [setFocus, setValue])
+
+  function onSubmit(values: FormValues) {
+    if (values.otp !== '123   ') {
+      window.alert('Invalid OTP')
+      reset()
+      return
+    }
+    window.alert('Valid OTP')
+  }
+
+  const getSlotClassnames = (isFocused: boolean, isSlotActive: boolean) =>
+    cn(
+      'relative w-10 md:w-20 h-14 md:h-28 text-[2rem] lg:text-[4rem] flex items-center justify-center border-border border-y border-r first:border-l first:rounded-l-md last:rounded-r-md transition-all [transition-duration:300ms] [--bsh-width:0] [box-shadow:0_0_0_var(--bsh-width)_hsl(var(--accent-foreground)_/_1)]',
+      {
+        'border-accent-foreground/20': isFocused,
+        '[--bsh-width:4px] z-10': isSlotActive,
+        'opacity-50': formState.disabled,
+      },
+    )
+
+  return (
+    <form
+      className={cn(
+        'mx-auto flex max-w-[980px] justify-center pt-6 pb-4',
+        className,
+      )}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <Controller
+        name="otp"
+        control={control}
+        render={({ field }) => (
+          <OTPInput
+            {...field}
+            maxLength={6}
+            // regexp={null} // Allow everything
+            render={({ triggerProps, slots, isFocused }) => (
+              <button {...triggerProps} className="flex items-center">
+                <div className="flex">
+                  {slots.slice(0, 3).map((slot, idx) => (
+                    <div
+                      key={idx}
+                      className={getSlotClassnames(isFocused, slot.isActive)}
+                    >
+                      <div className="">{slot.char || ' '}</div>
+
+                      {slot.isActive && slot.char === null && <FakeCaret />}
+                    </div>
+                  ))}
+                </div>
+
+                {/* Layout inspired by Stripe */}
+                <div className="flex w-10 justify-center items-center">
+                  <div className="w-3 h-1 rounded-full bg-border"></div>
+                </div>
+
+                <div className="flex">
+                  {slots.slice(3).map((slot, idx) => (
+                    <div
+                      key={idx}
+                      className={getSlotClassnames(isFocused, slot.isActive)}
+                    >
+                      {slot.char || ' '}
+
+                      {slot.isActive && slot.char === null && <FakeCaret />}
+                    </div>
+                  ))}
+                </div>
+              </button>
+            )}
+          />
+        )}
+      />
+    </form>
+  )
+}
+
+function FakeCaret() {
+  return (
+    <div className="absolute pointer-events-none inset-0 flex items-center justify-center animate-caret-blink">
+      <div className="w-px h-8 md:w-0.5 md:h-16 bg-white" />
+    </div>
+  )
+}
---
+++ src/app/(pages)/(home)/page.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import { cn } from '../../../lib/utils/cn'
+import { Controller, useForm } from 'react-hook-form'
+import { OTPInput } from '@/app/component'
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from '@/components/page-header'
+import Link from 'next/link'
+import { Icons } from '@/components/icons'
+import { buttonVariants } from '@/components/ui/button'
+import { siteConfig } from '@/config/site'
+import { Showcase } from './_components/showcase'
+
+export default function IndexPage() {
+  const fadeUpClassname = 'motion-safe:opacity-0 motion-safe:animate-fade-up'
+
+  return (
+    <div className="container relative flex-1 flex flex-col justify-center">
+      <PageHeader>
+        <PageHeaderHeading className={cn(fadeUpClassname)}>
+          Stop wasting time building OTP inputs.
+        </PageHeaderHeading>
+
+        <Showcase className={cn(fadeUpClassname, 'motion-safe:delay-1000')} />
+
+        <PageHeaderDescription className={cn(fadeUpClassname, 'motion-safe:delay-1500')}>
+          One-time password input component for React. Accessible. Unstyled.
+          Customizable. Open Source.
+        </PageHeaderDescription>
+
+        <PageActions className={cn(fadeUpClassname, 'motion-safe:delay-1500')}>
+          <Link href="/docs" className={cn(buttonVariants())}>
+            Get Started
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.github}
+            className={cn(buttonVariants({ variant: 'outline' }))}
+          >
+            <Icons.gitHub className="mr-2 h-4 w-4" />
+            GitHub
+          </Link>
+        </PageActions>
+      </PageHeader>
+    </div>
+  )
+}
