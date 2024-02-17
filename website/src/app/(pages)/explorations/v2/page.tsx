08558ee39a83c142d835eafe2bd3e3f6a9194c44
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-02-17 13:46:44 -0300

    chore(v2): add multiple input exploration

--- website/src/app/(pages)/explorations/v2/page.tsx
+++ website/src/app/(pages)/explorations/v2/page.tsx
@@ -7,13 +7,48 @@ export default function OtherPage() {
 
   return (
     <div className="flex flex-col w-full h-full items-center justify-center !text-black">
-      <InputEngine value={value} setValue={setValue} maxLength={6} />
+      <CustomInput />
+      <input />
+      {/* <InputEngine value={value} setValue={setValue} maxLength={6} />
       <InputEngine
         value={value}
         setValue={setValue}
         maxLength={6}
         allowNavigation={false}
-      />
+      /> */}
+    </div>
+  )
+}
+
+// Multiple inputs/div[contenteditable] exploration
+function CustomInput() {
+  // Should come as props
+  const [value, setValue] = React.useState<string>('123456')
+  const maxLength = 6
+
+  // State, ref and memo
+
+  // Effects
+
+  // Handlers
+
+  // Pre-render calculations
+  const slots = Array.from({ length: maxLength }, (_, i) => i)
+
+  const firstEmptySlotIdx = slots.findIndex((_, idx) => value[idx] === undefined || value[idx] === null)
+  const currentSlot = firstEmptySlotIdx === -1 ? 0 : firstEmptySlotIdx
+  
+  // Render
+  return (
+    <div className="flex gap-1" role="textbox">
+      {slots.map(item => (
+        <input
+          key={item}
+          tabIndex={currentSlot === item ? 0 : -1}
+          className="bg-white w-10 h-14"
+          role="none"
+        />
+      ))}
     </div>
   )
 }
@@ -45,179 +80,201 @@ type InputProps = {
  * - When the input is focused and ready, the uiSelection is never `[null,null]`
  * - When `isReady===false`, the input is NEVER modifiable by user native trusted events
  */
-function InputEngine({
-  value,
-  setValue,
-  maxLength,
-  allowNavigation = true,
-  ...props
-}: InputProps) {
-  /** States, Refs and Memos */
-  const inputRef = React.useRef<HTMLInputElement>(null)
-  // Is the input ready to be used by the user?
-  const [isReady, setIsReady] = React.useState<boolean>(false)
-  // Shouldn't be used for any logic. Used for UI rendering purposes only.
-  const [mirror, updateMirror] = React.useState<Selection>([null, null])
-  const metadata = React.useRef<Metadata>({
-    latestInputSelection: [null, null],
-  })
+const InputEngine = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ value, setValue, maxLength, allowNavigation = true, ...props }, ref) => {
+    /** States, Refs and Memos */
+    const inputRef = React.useRef<HTMLInputElement>(null)
+    React.useImperativeHandle(
+      ref,
+      () => {
+        const el = inputRef.current as HTMLInputElement
 
-  /** Effects */
+        const _select = el.select.bind(el)
+        el.select = () => {
+          if (!allowNavigation) {
+            return
+          }
 
-  /** Utils */
-  const setInputSelectionRange = React.useCallback(
-    (start: SelectionValue, end: SelectionValue, newValue?: string) =>
-      inputRef.current
-        ? _setClampedSelectionRange(
-            inputRef.current,
-            start,
-            end,
-            maxLength,
-            newValue ?? value,
-          )
-        : void 0,
-    [maxLength, value],
-  )
+          console.log('selecting')
 
-  const handleSelection = React.useCallback(
-    (sel: Selection, newValue?: string) => {
+          _select()
+          // Proxy to update the caretData
+          updateMirror([0, el.value.length])
+        }
+        return el
+      },
+      [allowNavigation],
+    )
+    // Is the input ready to be used by the user?
+    const [isReady, setIsReady] = React.useState<boolean>(false)
+    // Shouldn't be used for any logic. Used for UI rendering purposes only.
+    const [mirror, updateMirror] = React.useState<Selection>([null, null])
+    const metadata = React.useRef<Metadata>({
+      latestInputSelection: [null, null],
+    })
+
+    /** Effects */
+
+    /** Utils */
+    const setInputSelectionRange = React.useCallback(
+      (start: SelectionValue, end: SelectionValue, newValue?: string) =>
+        inputRef.current
+          ? _setClampedSelectionRange(
+              inputRef.current,
+              start,
+              end,
+              maxLength,
+              newValue ?? value,
+            )
+          : void 0,
+      [maxLength, value],
+    )
+
+    const handleSelection = React.useCallback(
+      (sel: Selection, newValue?: string) => {
+        if (!inputRef.current) {
+          return
+        }
+
+        if (sel[0] === null || sel[1] === null) {
+          return
+        }
+
+        if (!allowNavigation && Math.abs(sel[0] - sel[1]) > 1) {
+          console.log('should prevent selection')
+          setInputSelectionRange(value.length - 1, value.length)
+        }
+        const isEventZeroSelection = sel[0] === sel[1]
+
+        if (isEventZeroSelection && sel[0] !== null && sel[1] !== null) {
+          const caretPosition = sel[0]
+
+          // console.log({ caretPosition, maxLength })
+
+          if (value.length === 0) {
+            // Do nothing
+          } else if (caretPosition === maxLength) {
+            setInputSelectionRange(maxLength - 1, maxLength, newValue)
+          } else if (
+            caretPosition === value.length &&
+            caretPosition < maxLength
+          ) {
+            // Do nothing
+          } else if (caretPosition === 0) {
+            setInputSelectionRange(0, 1, newValue)
+          } else {
+            const prevSel = metadata.current.latestInputSelection
+            if (prevSel) {
+              const isLeftDirection = caretPosition === prevSel[0]
+              if (isLeftDirection) {
+                setInputSelectionRange(caretPosition - 1, caretPosition)
+              } else {
+                setInputSelectionRange(caretPosition, caretPosition + 1)
+              }
+            }
+          }
+
+          setIsReady(true)
+          updateMirror([
+            inputRef.current.selectionStart,
+            inputRef.current.selectionEnd,
+          ])
+        }
+      },
+      [maxLength, setInputSelectionRange, value.length],
+    )
+
+    function forceRecalculateSelection() {
+      inputRef.current &&
+        handleSelection([
+          inputRef.current.selectionStart,
+          inputRef.current.selectionEnd,
+        ])
+    }
+
+    /** Handlers */
+    function handleInputKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {}
+    function handleInputKeyDown(e: React.KeyboardEvent) {
+      console.log(e.key)
+      if (SPECIAL_KEYS.includes(e.key)) {
+        setIsReady(false)
+        syncTimeoutWorkaround(forceRecalculateSelection)
+      }
+
+      if (!allowNavigation && NAVIGATION_KEYS.includes(e.key)) {
+        e.preventDefault()
+      }
+    }
+    function handleInputSelect(e: React.SyntheticEvent<HTMLInputElement>) {
+      const eventSel: Selection = [
+        e.currentTarget.selectionStart,
+        e.currentTarget.selectionEnd,
+      ]
+
+      handleSelection(eventSel)
+
+      // Update metadata
+      metadata.current.latestInputSelection = eventSel
+    }
+    function handleInputFocus(e: React.FocusEvent<HTMLInputElement>) {
+      setIsReady(false)
+      setInputSelectionRange(value.length - 1, value.length)
+      setIsReady(true)
+    }
+    function handleInputBlur(e: React.FocusEvent<HTMLInputElement>) {}
+    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+      const isInserting = e.target.value.length > value.length
+
+      e.preventDefault()
+
+      if (isReady) {
+        const newValue = e.target.value.slice(0, maxLength)
+
+        const regexp = /.*/ // TODO: use props.regex
+        const tested = regexp.test(newValue)
+
+        if (tested) {
+          setIsReady(false)
+          setValue(newValue)
+        }
+      } else {
+        // Do nothing
+      }
+    }
+
+    React.useEffect(() => {
       if (!inputRef.current) {
         return
       }
 
-      if (sel[0] === null || sel[1] === null) {
-        return
-      }
+      handleSelection(
+        [inputRef.current.selectionStart, inputRef.current.selectionEnd],
+        value,
+      )
+    }, [handleSelection, value])
 
-      const isEventZeroSelection = sel[0] === sel[1]
-
-      if (isEventZeroSelection && sel[0] !== null && sel[1] !== null) {
-        const caretPosition = sel[0]
-
-        // console.log({ caretPosition, maxLength })
-
-        if (value.length === 0) {
-          // Do nothing
-        } else if (caretPosition === maxLength) {
-          setInputSelectionRange(maxLength - 1, maxLength, newValue)
-        } else if (
-          caretPosition === value.length &&
-          caretPosition < maxLength
-        ) {
-          // Do nothing
-        } else if (caretPosition === 0) {
-          setInputSelectionRange(0, 1, newValue)
-        } else {
-          const prevSel = metadata.current.latestInputSelection
-          if (prevSel) {
-            const isLeftDirection = caretPosition === prevSel[0]
-            if (isLeftDirection) {
-              setInputSelectionRange(caretPosition - 1, caretPosition)
-            } else {
-              setInputSelectionRange(caretPosition, caretPosition + 1)
-            }
-          }
-        }
-
-        setIsReady(true)
-        updateMirror([
-          inputRef.current.selectionStart,
-          inputRef.current.selectionEnd,
-        ])
-      }
-    },
-    [maxLength, setInputSelectionRange, value.length],
-  )
-
-  function forceRecalculateSelection() {
-    inputRef.current &&
-      handleSelection([
-        inputRef.current.selectionStart,
-        inputRef.current.selectionEnd,
-      ])
-  }
-
-  /** Handlers */
-  function handleInputKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {}
-  function handleInputKeyDown(e: React.KeyboardEvent) {
-    console.log(e.key)
-    if (SPECIAL_KEYS.includes(e.key)) {
-      setIsReady(false)
-      syncTimeoutWorkaround(forceRecalculateSelection)
-    }
-
-    if (!allowNavigation && NAVIGATION_KEYS.includes(e.key)) {
-      e.preventDefault()
-    }
-  }
-  function handleInputSelect(e: React.SyntheticEvent<HTMLInputElement>) {
-    const eventSel: Selection = [
-      e.currentTarget.selectionStart,
-      e.currentTarget.selectionEnd,
-    ]
-
-    handleSelection(eventSel)
-
-    // Update metadata
-    metadata.current.latestInputSelection = eventSel
-  }
-  function handleInputFocus(e: React.FocusEvent<HTMLInputElement>) {
-    setIsReady(false)
-    setInputSelectionRange(value.length - 1, value.length)
-    setIsReady(true)
-  }
-  function handleInputBlur(e: React.FocusEvent<HTMLInputElement>) {}
-  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const isInserting = e.target.value.length > value.length
-
-    e.preventDefault()
-
-    if (isReady) {
-      const newValue = e.target.value.slice(0, maxLength)
-
-      const regexp = /.*/ // TODO: use props.regex
-      const tested = regexp.test(newValue)
-
-      if (tested) {
-        setIsReady(false)
-        setValue(newValue)
-      }
-    } else {
-      // Do nothing
-    }
-  }
-
-  React.useEffect(() => {
-    if (!inputRef.current) {
-      return
-    }
-
-    handleSelection(
-      [inputRef.current.selectionStart, inputRef.current.selectionEnd],
-      value,
+    /** Render */
+    return (
+      <input
+        // Visual
+        className="w-1/2 h-12"
+        // Ref
+        ref={inputRef}
+        // Props
+        value={value}
+        maxLength={maxLength}
+        // Events
+        onChange={handleInputChange}
+        onSelect={handleInputSelect}
+        onKeyDown={handleInputKeyDown}
+        onKeyUp={handleInputKeyUp}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+      />
     )
-  }, [handleSelection, value])
-
-  /** Render */
-  return (
-    <input
-      // Visual
-      className="w-1/2 h-12"
-      // Ref
-      ref={inputRef}
-      // Props
-      value={value}
-      maxLength={maxLength}
-      // Events
-      onChange={handleInputChange}
-      onSelect={handleInputSelect}
-      onKeyDown={handleInputKeyDown}
-      onKeyUp={handleInputKeyUp}
-      onFocus={handleInputFocus}
-      onBlur={handleInputBlur}
-    />
-  )
-}
+  },
+)
+InputEngine.displayName = 'InputEngine'
 
 function IsolatedInput() {}
 
