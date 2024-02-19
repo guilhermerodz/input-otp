cd79dc21a8dc29d5136cb021ad016ffef575784d
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-02-19 17:50:44 -0300

    chore(website): remove text-balance from heading

--- website/src/components/page-header.tsx
+++ website/src/components/page-header.tsx
@@ -25,7 +25,7 @@ function PageHeaderHeading({
   return (
     <h1
       className={cn(
-        "text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1] text-balance",
+        "text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1] max-w-[330px] md:max-w-[500px] ",
         className
       )}
       {...props}
