95531c8a4e36a93b92edbcf14e87bb4e1ae1922c
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-02-19 17:08:43 -0300

    chore(website/playground): center layout

--- website/src/app/(local-pages)/example-playground/page.tsx
+++ website/src/app/(local-pages)/example-playground/page.tsx
@@ -2,7 +2,7 @@ import { ExampleComponent } from './component'
 
 export default function ExamplePlayground() {
   return (
-    <div className="container relative flex-1 flex flex-col justify-center">
+    <div className="container relative flex-1 flex flex-col justify-center items-center">
       <ExampleComponent />
     </div>
   )
