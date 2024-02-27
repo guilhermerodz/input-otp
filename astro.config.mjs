35a118106c305cfd377a6bc04249a89f88827bbd
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-02-27 17:24:03 -0300

    chore(playground): log form data

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
   integrations: [
     tailwind({
       applyBaseStyles: false,
