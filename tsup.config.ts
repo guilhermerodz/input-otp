eab4761177004caf4581867494a1b32ff54346ac
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-02-29 08:54:04 -0300

    build: vue

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsup';
 
-import esbuildVue from 'esbuild-plugin-vue3'
+import esbuildVue from 'unplugin-vue/esbuild'
+// import esbuildVue from 'esbuild-plugin-vue3'
 import esbuildSvelte from 'esbuild-svelte'
 
 import sveltePreprocess from "svelte-preprocess";
@@ -11,20 +12,36 @@ export default defineConfig({
   tsconfig: './tsconfig.build.json',
 
   external: ['react', 'vue', 'svelte'],
-  entry: ['!lib/dist/package.json', 'lib/index.ts', 'lib/react/index.ts'],
+  entry: [
+    '!lib/dist/package.json',
+    'lib/index.ts',
+    'lib/react/index.ts',
+    'lib/vue/index.ts',
+    'lib/svelte/index.ts'
+  ],
+  dts: {
+    entry: [
+      'lib/index.ts',
+      'lib/react/index.ts',
+      // 'lib/vue/index.ts', // Doesn't work
+      // 'lib/svelte/index.ts', // Doesn't work
+    ],
+  },
   outDir: 'lib-dist',
   format: ['cjs', 'esm'],
   sourcemap: true,
   publicDir: false,
-  dts: true, // Fails with vue
   // minify: true,
 
   esbuildPlugins: [
-    esbuildVue(),
+    esbuildVue({
+      isProduction: true,
+    }),
     esbuildSvelte({
       preprocess: sveltePreprocess({
         typescript: {
-          tsconfigFile: './tsconfig.build',
+          tsconfigDirectory: './',
+          tsconfigFile: './tsconfig.build.json',
           // compilerOptions: {
           //   "preserveValueImports": false,
           //   "verbatimModuleSyntax": false,
