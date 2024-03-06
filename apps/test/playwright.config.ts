9be2f489379f68815fb6a4f9a31a131612862ec9
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-03-05 23:55:49 -0300

    test: add standard browsers

--- apps/test/playwright.config.ts
+++ apps/test/playwright.config.ts
@@ -46,14 +46,42 @@ export default defineConfig({
     reuseExistingServer: !process.env.CI,
   },
   /* Configure projects for major browsers */
-  // projects: [
-  //   {
-  //     name: 'iPhone',
-  //     use: { ...devices['iPhone 13 Pro'] },
-  //   },
-  //   {
-  //     name: 'Pixel',
-  //     use: { ...devices['Pixel 5'] },
-  //   },
-  // ],
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'], contextOptions: {} },
+    },
+
+    /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+
+    /* Test against branded browsers. */
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
+  ],
 })
