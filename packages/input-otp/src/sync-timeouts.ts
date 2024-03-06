8b6f6298dea9be584ecf791e3ced47842657636b
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-03-06 00:08:48 -0300

    style: run linting

--- packages/input-otp/src/sync-timeouts.ts
+++ packages/input-otp/src/sync-timeouts.ts
@@ -2,7 +2,7 @@ export function syncTimeouts(cb: (...args: any[]) => unknown): number[] {
   const t1 = setTimeout(cb, 0) // For faster machines
   const t2 = setTimeout(cb, 1_0)
   const t3 = setTimeout(cb, 5_0)
-  return [t1,t2,t3]
+  return [t1, t2, t3]
 
   // const t3 = setTimeout(cb, 5_0)
   // return [t3]
