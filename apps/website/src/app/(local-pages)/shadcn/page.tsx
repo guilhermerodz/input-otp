4a71419cc448af4ee6fd1609d1c31ab8843ad780
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-03-07 01:59:55 -0300

    chore(website/local): remove unused lines

--- apps/website/src/app/(local-pages)/shadcn/page.tsx
+++ apps/website/src/app/(local-pages)/shadcn/page.tsx
@@ -13,21 +13,9 @@ export default function ShadcnPage() {
 
   return (
     <div className="container relative flex-1 flex flex-col justify-center items-center">
-      <input
-        type="text"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-        maxLength={6}
-        onInput={e => {
-          window.alert(e.currentTarget.value)
-        }}
-      />
       <InputOTP
         value={value}
         onChange={setValue}
-        // onInput={e => {
-        //   window.alert(e.currentTarget.value)
-        // }}
         maxLength={6}
         render={({ slots }) => (
           <>
