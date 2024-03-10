48b16de80343b42db4355593890cfb8c98a39396
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-03-10 03:15:16 -0300

    test: fix selectors

--- apps/test/src/tests/base.props.spec.ts
+++ apps/test/src/tests/base.props.spec.ts
@@ -6,15 +6,15 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Props tests', () => {
   test('should receive props accordingly', async ({ page }) => {
-    const input1 = page.getByTestId('input-otp-1').getByRole('textbox')
-    const input2 = page.getByTestId('input-otp-2').getByRole('textbox')
-    const input3 = page.getByTestId('input-otp-3').getByRole('textbox')
+    const input1 = page.getByTestId('input-otp-1')
+    const input2 = page.getByTestId('input-otp-2')
+    const input3 = page.getByTestId('input-otp-3')
     const container4 = page.locator(
       `[data-input-otp-container]:has([data-testid="input-otp-4"])`,
     )
-    const input5 = page.getByTestId('input-otp-5').getByRole('textbox')
-    const input6 = page.getByTestId('input-otp-6').getByRole('textbox')
-    const input7 = page.getByTestId('input-otp-7').getByRole('textbox')
+    const input5 = page.getByTestId('input-otp-5')
+    const input6 = page.getByTestId('input-otp-6')
+    const input7 = page.getByTestId('input-otp-7')
 
     await expect(input1).toBeDisabled()
 
