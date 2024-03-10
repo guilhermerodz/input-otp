80973315b38194debe1131e220231660c2a44eec
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-03-10 03:15:38 -0300

    ci: set test retries to 2 from 1

--- apps/test/playwright.config.ts
+++ apps/test/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
